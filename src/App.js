9cbf3d572e8f46778c668493b714f3f7ff094f62
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-09 11:41:12 +0100

    add solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: "1",
+      name: "Banana",
+      color: "green",
+      reason: "breakfast",
+      price: "1 Euro",
+    },
+    {
+      id: "2",
+      name: "Kirsche",
+      color: "orange",
+      reason: "summer",
+      price: "3,99 Euro",
+    },
+  ];
 
   return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
+    <ul className="app">
+      {fruits.map((fruit) => {
+        return (
+          <li key={fruit.id}>
+            <Card
+              name={fruit.name}
+              color={fruit.color}
+              reason={fruit.reason}
+              price={fruit.price}
+            />
+          </li>
+        );
+      })}
+    </ul>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,13 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ name, color, price, reason }) {
+  console.log(name);
+  return (
+    <article className={name === "Banana" ? "card--green" : "card--red"}>
+      <h2>{name}</h2>
+      <p>{color}</p>
+      <p>{price}</p>
+      <p>{reason}</p>
+    </article>
+  );
 }
